fd3fc1b3d7641997cd255d5d8418951784860552
Tim Randall <trtimrandall@gmail.com>
2021-07-15 23:45:59 +1000

    last commit

--- toDoSave/ContentView.swift
+++ toDoSave/ContentView.swift
@@ -11,18 +11,41 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @State var taskName: String = ""
+    @FetchRequest(
+        entity: Task.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Task.dateAdded, ascending: false)],
+        predicate: NSPredicate(format: "isComplete == %@", NSNumber(value: false))
+    ) var notCompletedTasks: FetchedResults<Task>
     var body: some View {
-        HStack{
-            TextField("Task Name", text:$taskName)
-            Button(action: {
-                self.addTask()
-            }){
+        VStack{
+            HStack{
+                TextField("Task Name", text:$taskName)
+                Button(action: {
+                    self.addTask()
+                }){
                 Text("add task")
             }
             }
+            List {
+                Text("HI")
+            }
         }
-    
     }
+    func addTask() {
+        let newTask = Task(context: viewContext)
+        newTask.id = UUID()
+        newTask.isComplete = false
+        newTask.name = taskName
+        newTask.dateAdded = Date()
+        
+        do {
+        try viewContext.save()
+        } catch {
+        print(error)
+        }
+    }
+
+}
 
 
 struct ContentView_Previews: PreviewProvider {
